b4fa38c907d2f95482a1cfd9b1fbbc8d545bd3a0
Tambi Asawo <tambi.asawo@yahoo.com>
2020-11-07 21:17:38 -0800

    fetchexercise2

--- src/client/js/footerBtn.js
+++ src/client/js/footerBtn.js
@@ -1,9 +1,9 @@
 const zip = 98101;
-const APIUnits = '33d88ff4a1f7f80be70bc95ed99e2434';
+const APIKey = '33d88ff4a1f7f80be70bc95ed99e2434';
 
 function clickButton(event) {
     event.preventDefault()
-    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=${APIUnits}&appid=${APIKey}`).
+    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${APIKey}`).
     then(response => response.json(). then(function(response){alert(response.sys.country)}))
 }
 
